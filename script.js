0dfd1ae90e140dc543571ad21f0bc35ced018ef1
Amarsingh348 <160129065+Amarsingh348@users.noreply.github.com>
2025-05-03 16:07:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function addTask() {
+const input = document.getElementById('taskInput');
+const taskText = input.value.trim();
+if (taskText === '') return;
+
+const li = document.createElement('li');
+li.innerHTML = `
+<span>${taskText}</span>
+<button onclick="this.parentElement.remove()">Delete</button>
+`;
+document.getElementById('taskList').appendChild(li);
+input.value = '';
+}
+
